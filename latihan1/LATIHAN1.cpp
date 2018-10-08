a169436c29fab47dcba28c865d2dc8e1193da4e5
alivia1919 <ibnu.muhammad987@gmail.com>
2018-10-08 10:38:25 +0700

    add latihan 1

---
+++ latihan1/LATIHAN1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+  int main() {
+    int A, T;
+    float L;
+
+    A = 5;
+    T = 7;
+
+    L = A * T / 2.0;
+
+    std::cout << "program menghitung luas segitiga" << std::endl;
+    std::cout << "alas segitiga = " << A << std::endl;
+    std::cout << "tinggi segitiga = " << T << std::endl;
+    std::cout << "luas segitiga adalah = " << L << std::endl;
+
+}
+
