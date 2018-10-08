b99015df58e93d4def7974df1f7408a3c1439aa1
alivia1919 <ibnu.muhammad987@gmail.com>
2018-10-08 11:07:19 +0700

    add latihan2

---
+++ LATIHAN2/latihan2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main() {
+        int A,B;
+        std::cout << "masukan nilai A: "; std::cin >> A;
+        std::cout << "masukan nilai B: "; std::cin >> B;
+
+
+        if (A > B) {
+        std::cout << A << " lebih besar dari " << B >> std::endl;
+        std::cout << B << " lebih kecil dari " << A >> std::endl;
+        } else
+        std::cout << B << " lebih besar dari " << A << std::endl;
+        }
